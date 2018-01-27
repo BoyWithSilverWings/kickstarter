e513f60bf20bd41de7053fce6b1427815365df15
BoyWithSilverWings <agney.menons@live.com>
2018-01-27 15:56:53 +0530

    Search filter UI in place

--- src/FilterBar.jsx
+++ src/FilterBar.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Grid, Input, Icon } from 'semantic-ui-react';
 
 class FilterBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
-      <p>Filter Bar</p>
+      <Grid container className='filter-bar'>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Input icon placeholder='Search...' fluid transparent>
+              <input />
+              <Icon name='search' />
+            </Input>
+          </Grid.Column>
+          <Grid.Column>
+            <p>Filter</p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     );
   }
 }
