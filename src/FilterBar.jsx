a9b3f2a1801fe79827a4d0962ee174f53178ff54
BoyWithSilverWings <agney.menons@live.com>
2018-01-27 14:43:56 +0530

    Filter Bar initial

---
+++ src/FilterBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class FilterBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <p>Filter Bar</p>
+    );
+  }
+}
+
+export default FilterBar;
