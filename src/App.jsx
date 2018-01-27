f5fd17ccae4249355c2a9a63eaf4df4525940780
BoyWithSilverWings <agney.menons@live.com>
2018-01-27 18:11:04 +0530

    All filter done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {API_URL} from './constants';
 import Header from './Header';
 import CardContainer from './CardContainer';
 import FilterBar from './FilterBar';
+import applyFilters from './Filter/applyFilter';
 import "whatwg-fetch";
 
 class App extends Component {
@@ -14,19 +15,30 @@ class App extends Component {
       data: [],
       loading: false
     };
-    this.filters = {};
+    this.dataFromAPI = [];
+    this.filters = {
+      search: null,
+      sort: null,
+      filter: null
+    };
     this.addToFilter = this.addToFilter.bind(this);
   }
   componentDidMount() {
     this.setState({loading: true})
     fetch(API_URL)
       .then((res)=>res.json())
-      .then(res=>this.setState({data: res}))
+      .then(res=>{
+        this.dataFromAPI = res;
+        this.setState({data: res})
+      })
       .catch(err=>this.setState({err: true}))
       .then(()=>this.setState({loading: false}));
   }
   addToFilter(key, value) {
     this.filters[key] = value;
+    this.setState({
+      data: applyFilters(this.dataFromAPI, this.filters)
+    });
   }
   render() {
     return (
--- src/Filter/applyFilter.js
+++ src/Filter/applyFilter.js
@@ -1,14 +1,42 @@
-import {LOCATIONS} from '../constants';
+import {compareAsc} from 'date-fns';
 
 function search(data, keyword) {
-  return data.filter(item=>item.includes(keyword));
+  return data.filter(item=>item.title.toLowerCase().includes(keyword.toLowerCase()));
 }
 
 function sort(data, keyword, direction) {
-  return data.sort((a,b)=>a[keyword]-b[keyword]);
+  if(direction==='asc')
+    return data.sort((a,b) => a[keyword]-b[keyword]);
+  else 
+    return data.sort((a, b) => b[keyword] - a[keyword]);
 }
 
-function filter(data, keyword) {
-  return data.filter(item=>item.location===keyword);
+function sortByDate(data, keyword, direction) {
+  if(direction==='asc')
+    return data.sort((a,b) => compareAsc(a[keyword],b[keyword]));
+  else 
+    return data.sort((a, b) => compareAsc(b[keyword], a[keyword]));
 }
+
+function fliter(data, keyword) {
+  return data.filter(item => item.location===keyword);
+}
+
+function applyFilters(data, filter) {
+  const searchArray = filter.search?search(data, filter.search):data;
+  const filteredArray = filter.filter?fliter(searchArray, filter.filter):searchArray;
+  let sortedArray = filteredArray;
+  if(filter.sort) {
+    console.log(filter.sort);
+    if(filter.sort.key==='progress') {
+      sortedArray = sort(filteredArray, 'percentage.funded', filter.sort.value);
+    } 
+    else if(filter.sort.key==='end') {
+      sortedArray = sortByDate(filteredArray, 'end.time', new Date(filter.sort.value));
+    }
+  }
+  return sortedArray;
+}
+
+export default applyFilters;
 
