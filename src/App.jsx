1d2d05e8bd52c4bd1f5e8c1ec3466575f7598732
BoyWithSilverWings <agney.menons@live.com>
2018-01-27 12:13:03 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import React, { Component } from "react";
+import "whatwg-fetch";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ""
+    };
+  }
+  render() {
+    return (
+      <div>
+        <p>App</p>
+      </div>
+    );
+  }
+}
+export default App;
